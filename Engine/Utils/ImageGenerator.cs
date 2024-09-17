51e5cf7c21c34b33046a7041f5c93e1c76c571c5
Tommi Iversen <tomi@dr.dk>
2024-09-17 15:39:21 +0200

    dynamisk gradient

--- Engine/Utils/ImageGenerator.cs
+++ Engine/Utils/ImageGenerator.cs
@@ -9,8 +9,17 @@ public class ImageGenerator
     public byte[] GenerateImageWithNumber(int number)
     {
         // Definer størrelse på billedet
-        int width = 300;
-        int height = 220;
+        const int width = 300;
+        const int height = 220;
+        
+        // Beregn farver baseret på nummeret ved hjælp af modulus 255
+        int r1 = number % 255;
+        int g1 = (number * 2) % 255;
+        int b1 = (number * 3) % 255;
+
+        int r2 = (number + 100) % 255;
+        int g2 = (number + 150) % 255;
+        int b2 = (number + 200) % 255;
 
         // Opret et nyt bitmap billede
         using (Bitmap bitmap = new Bitmap(width, height))
@@ -21,8 +30,8 @@ public class ImageGenerator
                 // Opret en gradient baggrund (fra blå til hvid)
                 using (LinearGradientBrush gradientBrush = new LinearGradientBrush(
                            new Rectangle(0, 0, width, height),
-                           Color.FromArgb(71, 76, 255),   // Startfarve
-                           Color.FromArgb(255, 57, 98),  // Slutfarve
+                           Color.FromArgb(r1, g1, b1),   // Startfarve
+                           Color.FromArgb(r2, g2, b2),   // Slutfarve
                            LinearGradientMode.Horizontal)) // Retning af gradienten
                 {
                     // Fyld baggrunden med gradienten
