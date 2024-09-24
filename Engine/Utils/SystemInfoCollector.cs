93d5d1adcc9af5c59c49712197fa9d9dab54ed25
Tommi Iversen <tomi@dr.dk>
2024-09-24 12:01:01 +0200

    add SystemInfoModel.cscollector

---
+++ Engine/Utils/SystemInfoCollector.cs
@@ -0,0 +1,40 @@
+﻿using Common.Models;
+
+namespace Engine.Utils;
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+public class SystemInfoCollector
+{
+    public SystemInfoModel GetSystemInfo()
+    {
+        return new SystemInfoModel
+        {
+            OSName = RuntimeInformation.OSDescription,
+            OSVersion = Environment.OSVersion.ToString(),
+            Architecture = RuntimeInformation.OSArchitecture.ToString(),
+            Uptime = GetSystemUptime(),
+            ProcessCount = Process.GetProcesses().Length,
+            Platform = GetPlatform()
+        };
+    }
+
+    private double GetSystemUptime()
+    {
+        return TimeSpan.FromMilliseconds(Environment.TickCount64).TotalSeconds;
+    }
+
+    private string GetPlatform()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            return "Windows";
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            return "Linux";
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            return "macOS";
+
+        return "Unknown";
+    }
+}
