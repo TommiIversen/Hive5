ec76343b15a9c7a2874d725bbe78940634330892
Tommi Iversen <tomi@dr.dk>
2024-10-05 11:06:29 +0200

    add migrations filer

---
+++ Engine/Migrations/20241004194635_AddEngineEntity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Engine.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEngineEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EngineEntities",
+                columns: table => new
+                {
+                    EngineId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Version = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    InstallDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EngineEntities", x => x.EngineId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EngineEntities");
+        }
+    }
+}
