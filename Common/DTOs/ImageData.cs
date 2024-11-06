30ac7844859764cf6344d280cdd72c71f1f3cc5b
Tommi Iversen <tomi@dr.dk>
2024-11-06 10:59:19 +0100

    clean

--- Common/DTOs/ImageData.cs
+++ Common/DTOs/ImageData.cs
@@ -2,7 +2,7 @@
 
 public class ImageData : BaseMessage
 {
-    public required byte[] ImageBytes { get; set; }
-    public required string WorkerId { get; set; }
+    public required byte[] ImageBytes { get; init; }
+    public required string WorkerId { get; init; }
     public int ImageSequenceNumber { get; set; }
 }
